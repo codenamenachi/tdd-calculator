cb395950b2afec44c5382215a67e6cc36685fba5
nacchi93@live.com <nacchi93@live.com>
2021-01-09 00:43:55 +0530

    Code stage 5

--- src/main/java/add/Addition.java
+++ src/main/java/add/Addition.java
@@ -6,16 +6,15 @@ public class Addition {
 
     public int add(String numbers){
 
-        // For test 6
         String delimiter = numbers.split("\\n")[0] != null ? numbers.split("\\n")[0] : ",";
         int sum = 0;
         String negetives = "";
         for(String n : numbers.split(delimiter)){
             int x = Integer.parseInt(n);
-            if (x >= 0)
-                sum += Integer.parseInt(n);
-            else
-                negetives+=", "+x;
+            if (x >= 0) {
+                if(x <= 1000) sum += Integer.parseInt(n);
+            } else
+                negetives+=" "+x;
         }
         if(!negetives.isEmpty())
             throw new Exception("negatives not allowed - "+negetives);
